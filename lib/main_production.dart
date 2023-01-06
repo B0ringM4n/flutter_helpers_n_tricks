69bc3808eb22f12a77d0563b44e51dc61bb2e9e0
salvador miron ramos <smiron@bnext.mx>
2023-01-05 22:03:29 -0500

    buenas practicas

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,5 +1,5 @@
-import 'package:helpers_n_tricks/features/app/app.dart';
 import 'package:helpers_n_tricks/bootstrap.dart';
+import 'package:helpers_n_tricks/features/app/app.dart';
 
 void main() {
   bootstrap(() => const App());
