b3d68245c759dbb68f28bf814277dde155ae5d2c
salvador miron ramos <smiron@bnext.mx>
2023-01-05 20:07:32 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/helpers/n/tricks/softsea/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.helpers.n.tricks.softsea.helpers_n_tricks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:helpers_n_tricks/app/app.dart';
+import 'package:helpers_n_tricks/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
