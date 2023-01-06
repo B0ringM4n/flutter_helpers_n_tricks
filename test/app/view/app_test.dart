018c8fc1e22c1c3fda2c090e48b8620087c3e288
salvador miron ramos <smiron@bnext.mx>
2023-01-05 23:55:30 -0500

    correcion en app test

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:helpers_n_tricks/features/app/app.dart';
-import 'package:helpers_n_tricks/features/counter/counter.dart';
+import 'package:helpers_n_tricks/features/home/presentation/view/page.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(HomePage), findsOneWidget);
     });
   });
 }
